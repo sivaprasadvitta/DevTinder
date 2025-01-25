6017031e2184b4b9419c60bae78653b177055012
sivaprasadvitta <sivaprasad.vitta@gmail.com>
2025-01-25 19:23:21 +0530

    update app.js

--- src/app.js
+++ src/app.js
@@ -24,7 +24,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin:"https://devtinder-kroy.onrender.com" || "http://localhost:5173/",
+    origin:"https://devtinder-kroy.onrender.com",
     credentials:true
 }))
 
